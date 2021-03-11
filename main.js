375717c45d89ed7482c4e339f882b7ec0102d08b
Dora Budić <dbudic0706@gmail.com>
2021-03-11 15:21:55 +0100

    Slide to left

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   slideRight();
+  slideLeft();
 });
 
 // Scorro tra le immagini verso destra (arrow a destra)
@@ -11,7 +12,7 @@ function slideRight() {
   next.click(function () {
     // Prendo l'imagine visibile
     var image = $('.slider-wrapper > .images img.active');
-    // Prendo il primo immagine
+    // Prendo la prima immagine
     var firstImage = $('.slider-wrapper > .images img.first');
     // Prendo il pallino blue
     var circle = $('.slider-wrapper > .nav i.active');
@@ -19,7 +20,7 @@ function slideRight() {
     var firstCircle = $('.slider-wrapper > .nav i.first');
 
     // Faccio scomparire l'immagine selezionata e aparire la seguente,
-    // cambio il colore al pallino
+    // cambio il colore del pallino
     if (image.hasClass('last')) {
       image.removeClass('active');
       firstImage.addClass('active');
@@ -30,6 +31,39 @@ function slideRight() {
       image.next('img').addClass('active');
       circle.removeClass('active');
       circle.next('i').addClass('active');
+    }
+
+  });
+}
+
+// Scorro tra le immagini verso sinistra (arrow a sinistra)
+function slideLeft() {
+  // Prendo arrow left
+  var previous = $('.slider-wrapper > .prev .fa-angle-left');
+
+  // Click on arrow previous
+  previous.click(function () {
+    // Prendo l'imagine visibile
+    var image = $('.slider-wrapper > .images img.active');
+    // Prendo l'ultima immagine
+    var lastImage = $('.slider-wrapper > .images img.last');
+    // Prendo il pallino blue
+    var circle = $('.slider-wrapper > .nav i.active');
+    // Prendo l'ultimo pallino
+    var lastCircle = $('.slider-wrapper > .nav i.last');
+
+    // Faccio scomparire l'immagine selezionata e aparire la precedente,
+    // cambio il colore del pallino
+    if (image.hasClass('first')) {
+      image.removeClass('active');
+      lastImage.addClass('active');
+      circle.removeClass('active');
+      lastCircle.addClass('active');
+    } else {
+      image.removeClass('active');
+      image.prev('img').addClass('active');
+      circle.removeClass('active');
+      circle.prev('i').addClass('active');
     }
 
   });
